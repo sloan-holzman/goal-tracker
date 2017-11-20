0dd0de2c35739942bcd61123a28379cc7162430f
Sloan Holzman <sloan.holzman@gmail.com>
2017-11-20 13:21:54 -0500

    create basic navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,23 @@
 //= require turbolinks
 //= require_tree .
 //= require chartkick
+
+// Below is the dropdown taken from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
