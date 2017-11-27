6ee36056d4deffdf5b1889ca1f35d0f3fccb635c
Sloan Holzman <sloan.holzman@gmail.com>
2017-11-27 18:28:45 -0500

    reset gmail passwords to ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,10 +58,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   # :user_name            => ENV['gmail_username'],
-   # :password             => ENV['gmail_password'],
-   :user_name            => 'goaltracker2000@gmail.com',
-   :password             => 'project2',
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   # :user_name            => 'goaltracker2000@gmail.com',
+   # :password             => 'project2',
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
