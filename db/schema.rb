623b4c75b4382b10591ada15d824cef21f303bde
Sloan Holzman <sloan.holzman@gmail.com>
2017-11-27 23:05:11 -0500

    fixed migrations, so adding confirmable is its own migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127193147) do
+ActiveRecord::Schema.define(version: 20171128035942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,10 +82,6 @@ ActiveRecord::Schema.define(version: 20171127193147) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
@@ -93,6 +89,10 @@ ActiveRecord::Schema.define(version: 20171127193147) do
     t.boolean "reminder", default: true
     t.string "reminder_frequency", default: "Weekly"
     t.string "reminder_day", default: "Thursday"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
