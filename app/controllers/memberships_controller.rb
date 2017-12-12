ecaca309f37a79ccd3a6722815c43c36f385c83b
Sloan Holzman <sloan.holzman@gmail.com>
2017-12-12 12:34:41 -0500

    put group.admin_reassign into the model

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -8,13 +8,7 @@ class MembershipsController < ApplicationController
     @membership = Membership.find_by(user: @user, group: @group)
     @membership.destroy
     flash[:notice] = "You successfully left the group #{@group.name}"
-    if @group.users.length == 1 || @group.memberships.where(admin: true).count == 0
-      @admin = @group.users.first
-      @membership = Membership.where(group: @group, user: @admin)
-      @membership.update(admin: true)
-    else @group.users.length == 0
-      @group.destroy
-    end
+    @group.admin_reassign
     redirect_to user_groups_path(@user)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -10,5 +10,18 @@ class Group < ApplicationRecord
   before_save do |group|
     group.name = group.name.downcase.titleize
   end
-  
+
+  def admin_reassign
+    if self.users.length == 1 || self.memberships.where(admin: true).count == 0
+      admin = self.users.first
+      membership = Membership.where(group: self, user: admin)
+      membership.update(admin: true)
+    else self.users.length == 0
+      self.destroy
+    end
+  end
+
+
+
+
 end
