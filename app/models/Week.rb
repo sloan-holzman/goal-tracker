6d87dcb1205ef839eebed635ced14af67c354913
Sloan Holzman <sloan.holzman@gmail.com>
2018-01-02 09:11:04 -0500

    update week.rb file

--- app/models/Week.rb
+++ app/models/Week.rb
@@ -1,6 +1,5 @@
 class Week < ApplicationRecord
-  belongs_to :metric
+  belongs_to :group
   validates :date, :total, {presence: true}
   validates :total, numericality: { greater_than_or_equal_to: 0 }
-
 end
